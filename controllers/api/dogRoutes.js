e9292b2ecdd5cb21c24d236ba6628c1f7c3156ee
Badgers <brianbadger93@gmail.com>
2023-05-16 13:38:37 -0500

    fixed up dogroutes

--- controllers/api/dogRoutes.js
+++ controllers/api/dogRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Dog } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newDog = await Dog.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newDog);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const dogData = await Dog.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!dogData) {
+      res.status(404).json({ message: 'No dog found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(dogData);
   } catch (err) {
     res.status(500).json(err);
   }
