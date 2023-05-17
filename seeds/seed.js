d4323c898594420f53a98ca0a16cf2869d6f2d56
Jason Tavera <taverajason7@gmail.com>
2023-05-17 14:39:26 -0500

    fixed error

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,8 +3,8 @@ const { User, Dog, Vaccination, SpayNeuter } = require('../models');
 
 const userData = require('./userData.json');
 const dogData = require('./dogData.json');
-const Vaccination = require('./vaccinationData.json');
-const SpayNeuter = require('./spayNeuterData.json');
+const vaccineData = require('./vaccinationData.json');
+const spayNeuterData = require('./spayNeuterData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -14,12 +14,23 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  
   for (const dog of dogData) {
     await Dog.create({
       ...dog,
       // user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
+  
+  const vaccines = await Vaccination.bulkCreate(vaccineData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const spayAndNeuter = await SpayNeuter.bulkCreate(spayNeuterData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };
