233c307d2810b673fb5014fd120c3713249f5154
Jennifer Eckenrode <j.eckenrode@me.com>
2023-05-17 10:48:30 -0500

    created Adoption model and added its associations in index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,53 @@
 const User = require('./User');
 const Dog = require('./Dog');
+const Adoption = require('./Adoption')
 
+
+
+
+
+//Dog Associations
+Dog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Dog.hasMany(Adoption, {
+  foreignKey: 'dog_id'
+});
+
+Dog.hasMany(Vaccination, {
+  foreignKey: 'dog_id'
+});
+
+
+//User associations
 User.hasMany(Dog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Dog.belongsTo(User, {
+User.hasMany(Adoption, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Dog };
+
+//Adoption associations
+Adoption.belongsTo(Dog, {
+  foreignKey: 'dog_id'
+});
+
+Adoption.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+
+//Vaccination associations
+Vaccination.belongsTo(Dog, {
+  foreignKey: 'dog_id'
+});
+
+
+
+
+
+module.exports = { User, Dog, Adoption, Vaccination };
