b86268b726fc381b4daab4dd83248284501d43b0
Jennifer Eckenrode <j.eckenrode@me.com>
2023-05-17 23:00:39 -0500

    deleted profile.handlebars from views/layouts folder

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,14 +1,13 @@
-// script.js
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
   
-    // Retrieve form data
+
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var phone = document.getElementById('phone').value;
     var message = document.getElementById('message').value;
   
-    // Create data object with form values
+
     var data = {
       name: name,
       email: email,
