a322beb825f65ba38322b2505cc039c89d23e8ad
Jennifer Eckenrode <j.eckenrode@me.com>
2023-05-18 11:52:23 -0500

    changed endpoints from '/dogs' to 'adopt' for all dogs being rendered

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,18 +4,9 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    
-    const dogData = await Dog.findAll({
-    });
-
-    // Serialize data so the template can read it
-    const dogs = dogData.map((dog) => dog.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
 
     console.log(req.session)
-    res.render('homepage', { 
-      dogs, 
+    res.render('homepage', {  
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -92,17 +83,6 @@ router.get('/contact', async (req, res) => {
   }
 });
 
-router.get('/sign-up', async (req, res) => {
-  try {
-
-    // Pass serialized data and session flag into template
-    res.render('sign-up', { 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 router.get('/newdogs', async (req, res) => {
   try {
@@ -116,50 +96,6 @@ router.get('/newdogs', async (req, res) => {
   }
 });
 
-
-
-
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
