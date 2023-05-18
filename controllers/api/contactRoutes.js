20771292256601a1d1e4b11b72c1736710a053ca
Jennifer Eckenrode <j.eckenrode@me.com>
2023-05-17 22:48:36 -0500

    created contact.js

--- controllers/api/contactRoutes.js
+++ controllers/api/contactRoutes.js
@@ -1,14 +1,17 @@
 const router = require('express').Router();
 
-router.post('/submit-form' , (req, res) => {
-  // Process the form submission data
-  const { name, email, phone, message } = req.body;
+// Endpoint to process form submission
+router.post('/submit-form', async (req, res) => {
+  try {
+    const { name, email, phone, message } = req.body;
 
-  // Prepare the data for the confirmation page
-  const confirmationMessage = 'Thank you for your message. We will get back to you during our operating hours!';
+    const confirmationMessage = 'Thank you for your message. We will get back to you within 48 hours!';
 
-  // Render the confirmation page with the data
-  res.render('confirmation', { message: confirmationMessage });
+    res.render('confirmation', { message: confirmationMessage });
+  } catch (error) {
+    console.error(error);
+    res.render('error', { message: 'An error occurred during form submission' });
+  }
 });
 
 module.exports = router;
